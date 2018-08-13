d891fbde9926dc938e7878965e2724eef4839fac
Sebastian Weber <sebastian.weber@diva-e.com>
2018-08-13 18:39:26 +0200

    installation slide

--- src/slide-content/index.js
+++ src/slide-content/index.js
@@ -2,11 +2,12 @@ import overview from "./chapter-overview.json";
 import what from "./what.json";
 import github from "./github.json";
 import docu from "./docu.json";
+import installation from "./installation.json";
 import superior from "./superior-to-v2.json";
 import end from "./end.json";
 
 export default {
-  slides: [what, superior],
+  slides: [what, superior, installation],
   imageSlides: [end, github, docu],
   chapters: [overview],
   codepens: []
