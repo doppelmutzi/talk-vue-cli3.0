9a0c620fe93942907aad05bbee57426910e32e59
Sebastian Weber <sebastian.weber@diva-e.com>
2018-08-13 18:25:01 +0200

    Adding slides for github and docu

--- src/slide-content/index.js
+++ src/slide-content/index.js
@@ -1,11 +1,13 @@
 import overview from "./chapter-overview.json";
 import what from "./what.json";
+import github from "./github.json";
+import docu from "./docu.json";
 import superior from "./superior-to-v2.json";
 import end from "./end.json";
 
 export default {
   slides: [what, superior],
-  imageSlides: [end],
+  imageSlides: [end, github, docu],
   chapters: [overview],
   codepens: []
 };
