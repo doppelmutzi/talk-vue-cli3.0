806ac400557ad0cb9a00e9661e7d8ad7eaa6f79a
Sebastian Weber <sebastian.weber@diva-e.com>
2018-08-14 09:31:35 +0200

    Adding slides about vue-cli-service

--- src/slide-content/index.js
+++ src/slide-content/index.js
@@ -4,12 +4,21 @@ import github from "./github.json";
 import docu from "./docu.json";
 import installation from "./installation.json";
 import scaffold from "./scaffold.json";
+import vueCliService from "./vue-cli-service.json";
+import listVueCliServiceCommands from "./list-vue-cli-service-commands.json";
+import projectStructureCli3 from "./project-structure-cli3.json";
 import superior from "./superior-to-v2.json";
 import end from "./end.json";
 
 export default {
-  slides: [what, superior, installation],
-  imageSlides: [end, github, docu, scaffold],
+  slides: [
+    what,
+    superior,
+    installation,
+    vueCliService,
+    listVueCliServiceCommands
+  ],
+  imageSlides: [end, github, docu, scaffold, projectStructureCli3],
   chapters: [overview],
   codepens: []
 };
